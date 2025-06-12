ddeb524a787d1b06f8d06c7e8862f3ec6d3024d9
Pablo <pab.michelini@gmail.com>
2025-06-12 12:15:52 +0200

    light ilumination

--- Components/ThreePreview.tsx
+++ Components/ThreePreview.tsx
@@ -26,7 +26,7 @@ export default function ThreePreview({
 
   return (
     <Canvas camera={{ position: [0, 5, 15], fov: 45 }}>
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={1} />
       <directionalLight position={[5, 10, 5]} intensity={1} />
       <ShapeComponent texture={texture} meshRef={meshRef} />
     </Canvas>
